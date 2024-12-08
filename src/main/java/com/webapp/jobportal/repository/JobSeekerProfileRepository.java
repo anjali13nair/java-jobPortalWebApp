1a96bc9a75e3d7e79e19184d62e771ebd7ea2e00
ANJALI <anjalinnair13@gmail.com>
2024-12-09 03:13:23 +0530

    init commit - Job Portal Web Application code

---
+++ src/main/java/com/webapp/jobportal/repository/JobSeekerProfileRepository.java
@@ -0,0 +1,7 @@
+package com.webapp.jobportal.repository;
+
+import com.webapp.jobportal.entity.JobSeekerProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
+}
---
+++ src/main/java/com/webapp/jobportal/repository/RecruiterProfileRepository.java
@@ -0,0 +1,7 @@
+package com.webapp.jobportal.repository;
+
+import com.webapp.jobportal.entity.RecruiterProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RecruiterProfileRepository extends JpaRepository<RecruiterProfile, Integer> {
+}
